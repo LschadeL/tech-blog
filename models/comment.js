ea4ad16e250f51eb187dfc88c2ac84db71bd362e
Lillith Schade <loganniles96@gmail.com>
2024-04-15 02:38:08 -0500

    added initial files, began work on controllers

---
+++ models/comment.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: "Deleted User",
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "comment",
+        key: "id",
+      },
+    },
+    comment_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+module.exports = Comment;
